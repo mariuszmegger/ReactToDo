81b931c3363008378a7025d424a4d04ddbd3f43d
mariuszmegger <mariuszmegger@go2.pl>
2017-04-13 16:00:31 +0200

    Local Storage API

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,37 +1,21 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodoForm = require('AddTodoForm');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
     getInitialState: function(){
         return {
             showCompleted: false,
             searchText: '',
-            todos: [
-                {
-                    id:uuid(),
-                    text: 'walk the dog',
-                    completed:true
-                },
-                {
-                    id:uuid(),
-                    text:'clean the yard',
-                    completed:true
-                },
-                {
-                    id:uuid(),
-                    text:'clean the dishes',
-                    completed:false
-                },
-                {
-                    id:uuid(),
-                    text:'play with cats',
-                    completed:false
-                }
-            ]
+            todos: TodoAPI.getTodos()
         }
+    },
+    componentDidUpdate: function(){
+        TodoAPI.setTodos(this.state.todos);
     },
     handleAddTodo: function(text){
         var {todos} = this.state;
@@ -45,6 +29,7 @@ var TodoApp = React.createClass({
                 }
             ]
         })
+
     },
     handleToggle: function(id){
         var  updatedTodos = this.state.todos.map((todo) => {
