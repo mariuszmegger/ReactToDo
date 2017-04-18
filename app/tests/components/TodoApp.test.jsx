bd60029ca83951bf08f8dc7701419e4022a116dd
mariuszmegger <mariuszmegger@go2.pl>
2017-04-18 13:10:33 +0200

    Add completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,6 +19,7 @@ describe('TodoApp', () => {
         todoApp.handleAddTodo(todoText);
 
         expect(todoApp.state.todos[0].text).toBe(todoText);
+        expect(todoApp.state.todos[0].createdAt).toBeA('number');
     })
 
     it('should change completed value in cliked todo  component', () => {
@@ -27,15 +28,41 @@ describe('TodoApp', () => {
             {
                 id:'sdfsadfas',
                 text:'do Something',
-                completed: false
+                completed: false,
+                createdAt: 0,
+                completedAt: undefined
             },
         ];
 
         var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
         todoApp.setState({todos:todos});
         expect(todoApp.state.todos[0].completed).toBe(false);
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
         todoApp.handleToggle(todos[0].id);
 
         expect(todoApp.state.todos[0].completed).toBe(true);
+        expect(todoApp.state.todos[0].completedAt).toBeA('number');
+    })
+
+    it('should remove completedAt from  data object when completed app is clicked', () => {
+
+        var todos = [
+            {
+                id:'sdfsadfas',
+                text:'do Something',
+                completed: true,
+                createdAt: 1492511805,
+                completedAt: 1492511885
+            },
+        ];
+
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+        todoApp.setState({todos:todos});
+        expect(todoApp.state.todos[0].completed).toBe(true);
+        expect(todoApp.state.todos[0].completedAt).toBeA('number');
+        todoApp.handleToggle(todos[0].id);
+
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
     })
 })
