1912a81838bf33e0a63cbd3c87e666ef9dfe0215
mariuszmegger <mariuszmegger@go2.pl>
2017-04-20 15:11:25 +0200

    Reducers and actions for redux with tests

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,51 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+
+export var searchTextReducer = (state = '', action) => {
+    switch(action.type) {
+        case 'SET_SEARCH_TEXT':
+            return action.searchText;
+        default:
+            return state;
+    }
+}
+
+export var showCompletedReducer = (state = false, action) => {
+    switch(action.type) {
+        case 'TOGGLE_SHOW_COMPLETED':
+            return !state;
+        default:
+            return state;
+    }
+}
+
+export var toodosReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id:uuid(),
+                    text:action.text,
+                    completed:false,
+                    createdAt: moment().unix(),
+                    completedAt: undefined
+                }
+            ]
+        case 'TOGGLE_TODO':
+            return state.map((value) => {
+                if(value.id === action.id){
+                    var nextCompleted = !value.completed;
+
+                    return{
+                        ...value,
+                        completed: nextCompleted,
+                        completedAt: nextCompleted ? moment().unix() : undefined
+                    };
+                }
+            })
+        default:
+            return state;
+    }
+}
---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,65 @@
+var expect = require('expect');
+var reducers = require('reducers');
+var df = require('deep-freeze-strict');
+var uuid = require('node-uuid');
+var moment = require('moment');
+
+describe('Reducers', () => {
+    describe('searchTextReducer', () => {
+        it('should set search text', () => {
+            var action = {
+                type:'SET_SEARCH_TEXT',
+                searchText: 'dog'
+            }
+            var res = reducers.searchTextReducer(df(''), df(action));
+
+            expect(res).toEqual(action.searchText);
+        })
+    });
+
+    describe('showCompletedReducer', () => {
+        it('should set show completed', () => {
+            var action = {
+                type:'TOGGLE_SHOW_COMPLETED',
+            }
+            var res = reducers.showCompletedReducer(df(false), df(action));
+
+            expect(res).toEqual(true);
+        })
+    });
+
+    describe('toodosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type:'ADD_TODO',
+                text:'walk the dog'
+            }
+            var res = reducers.toodosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        })
+
+        it('should toggle todo', () => {
+
+             var todos = [
+                 {
+                     id:10,
+                     text:'eat breakfast',
+                     completed:false,
+                     createdAt: moment().unix(),
+                     completedAt: undefined
+                 }
+             ]
+
+            var action = {
+                type:'TOGGLE_TODO',
+                id:10
+            }
+
+            var res = reducers.toodosReducer(df(todos), df(action));
+            expect(res[0].completed).toEqual(true);
+            expect(res[0].completedAt).toExist();
+        })
+    });
+})
